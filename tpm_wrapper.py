5fc38a2df14d28c9fa95e3a1edf1cf5866d12ce1
Mike Ounsworth <mike@ounsworth.ca>
2024-03-17 14:36:57 +1000

    Updated DNs to be more official

--- tpm_wrapper.py
+++ tpm_wrapper.py
@@ -82,9 +82,9 @@ csr_builder = csr_builder.subject_name(x509.Name(
         x509.NameAttribute(NameOID.COUNTRY_NAME, 'AU'),
         x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, 'QLD'),
         x509.NameAttribute(NameOID.LOCALITY_NAME, 'Brisbane'),
-        x509.NameAttribute(NameOID.ORGANIZATION_NAME, 'ietf-csr-test'),
-        x509.NameAttribute(NameOID.ORGANIZATIONAL_UNIT_NAME, 'mwiseman-key1'),
-        x509.NameAttribute(NameOID.COMMON_NAME, 'mwiseman-key1-test')
+        x509.NameAttribute(NameOID.ORGANIZATION_NAME, 'ietf-119-hackathon'),
+        x509.NameAttribute(NameOID.ORGANIZATIONAL_UNIT_NAME, 'ietf-csr-test'),
+        x509.NameAttribute(NameOID.COMMON_NAME, 'key1')
     ]
 ))
 
