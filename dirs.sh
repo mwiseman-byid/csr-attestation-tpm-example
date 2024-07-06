e34fe2f2f7d7b78428e970eba6dfccf79bcae6f5
Monty Wiseman <monty.wiseman@beyondidentity.com>
2024-07-06 10:32:03 -0500

    Just changing order to make visual sense.

--- dirs.sh
+++ dirs.sh
@@ -1,7 +1,7 @@
 # /bin/bash
-# This is location for the Client.
-export cdir='./client'
 # This is location for the CA files.
 export cadir='./ca'
+# This is location for the Client.
+export cdir='./client'
 # This is the location for the Verifier's files.
 export vdir='./verifier'
